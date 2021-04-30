b0765ef74c41b280afed691ca9775d0517af7d79
YnneMagy <81574436+YnneMagy@users.noreply.github.com>
2021-04-30 08:36:03 +0200

    first half of data

---
+++ code0.js
@@ -0,0 +1,139 @@
+gdjs.New_32sceneCode = {};
+gdjs.New_32sceneCode.GDPlayerObjects1= [];
+gdjs.New_32sceneCode.GDPlayerObjects2= [];
+gdjs.New_32sceneCode.GDPlayerObjects3= [];
+gdjs.New_32sceneCode.GDTestBodenObjects1= [];
+gdjs.New_32sceneCode.GDTestBodenObjects2= [];
+gdjs.New_32sceneCode.GDTestBodenObjects3= [];
+
+gdjs.New_32sceneCode.conditionTrue_0 = {val:false};
+gdjs.New_32sceneCode.condition0IsTrue_0 = {val:false};
+gdjs.New_32sceneCode.condition1IsTrue_0 = {val:false};
+gdjs.New_32sceneCode.condition2IsTrue_0 = {val:false};
+
+
+gdjs.New_32sceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+gdjs.copyArray(gdjs.New_32sceneCode.GDPlayerObjects1, gdjs.New_32sceneCode.GDPlayerObjects2);
+
+
+gdjs.New_32sceneCode.condition0IsTrue_0.val = false;
+{
+for(var i = 0, k = 0, l = gdjs.New_32sceneCode.GDPlayerObjects2.length;i<l;++i) {
+    if ( !(gdjs.New_32sceneCode.GDPlayerObjects2[i].getBehavior("PlatformerObject").isMoving()) ) {
+        gdjs.New_32sceneCode.condition0IsTrue_0.val = true;
+        gdjs.New_32sceneCode.GDPlayerObjects2[k] = gdjs.New_32sceneCode.GDPlayerObjects2[i];
+        ++k;
+    }
+}
+gdjs.New_32sceneCode.GDPlayerObjects2.length = k;}if (gdjs.New_32sceneCode.condition0IsTrue_0.val) {
+/* Reuse gdjs.New_32sceneCode.GDPlayerObjects2 */
+{for(var i = 0, len = gdjs.New_32sceneCode.GDPlayerObjects2.length ;i < len;++i) {
+    gdjs.New_32sceneCode.GDPlayerObjects2[i].setAnimationName("Idle");
+}
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(gdjs.New_32sceneCode.GDPlayerObjects1, gdjs.New_32sceneCode.GDPlayerObjects2);
+
+
+gdjs.New_32sceneCode.condition0IsTrue_0.val = false;
+gdjs.New_32sceneCode.condition1IsTrue_0.val = false;
+{
+for(var i = 0, k = 0, l = gdjs.New_32sceneCode.GDPlayerObjects2.length;i<l;++i) {
+    if ( gdjs.New_32sceneCode.GDPlayerObjects2[i].getBehavior("PlatformerObject").isMoving() ) {
+        gdjs.New_32sceneCode.condition0IsTrue_0.val = true;
+        gdjs.New_32sceneCode.GDPlayerObjects2[k] = gdjs.New_32sceneCode.GDPlayerObjects2[i];
+        ++k;
+    }
+}
+gdjs.New_32sceneCode.GDPlayerObjects2.length = k;}if ( gdjs.New_32sceneCode.condition0IsTrue_0.val ) {
+{
+gdjs.New_32sceneCode.condition1IsTrue_0.val = gdjs.evtTools.input.isKeyPressed(runtimeScene, "Left");
+}}
+if (gdjs.New_32sceneCode.condition1IsTrue_0.val) {
+/* Reuse gdjs.New_32sceneCode.GDPlayerObjects2 */
+{for(var i = 0, len = gdjs.New_32sceneCode.GDPlayerObjects2.length ;i < len;++i) {
+    gdjs.New_32sceneCode.GDPlayerObjects2[i].setAnimationName("Runningl");
+}
+}}
+
+}
+
+
+{
+
+/* Reuse gdjs.New_32sceneCode.GDPlayerObjects1 */
+
+gdjs.New_32sceneCode.condition0IsTrue_0.val = false;
+gdjs.New_32sceneCode.condition1IsTrue_0.val = false;
+{
+for(var i = 0, k = 0, l = gdjs.New_32sceneCode.GDPlayerObjects1.length;i<l;++i) {
+    if ( gdjs.New_32sceneCode.GDPlayerObjects1[i].getBehavior("PlatformerObject").isMoving() ) {
+        gdjs.New_32sceneCode.condition0IsTrue_0.val = true;
+        gdjs.New_32sceneCode.GDPlayerObjects1[k] = gdjs.New_32sceneCode.GDPlayerObjects1[i];
+        ++k;
+    }
+}
+gdjs.New_32sceneCode.GDPlayerObjects1.length = k;}if ( gdjs.New_32sceneCode.condition0IsTrue_0.val ) {
+{
+gdjs.New_32sceneCode.condition1IsTrue_0.val = gdjs.evtTools.input.isKeyPressed(runtimeScene, "Right");
+}}
+if (gdjs.New_32sceneCode.condition1IsTrue_0.val) {
+/* Reuse gdjs.New_32sceneCode.GDPlayerObjects1 */
+{for(var i = 0, len = gdjs.New_32sceneCode.GDPlayerObjects1.length ;i < len;++i) {
+    gdjs.New_32sceneCode.GDPlayerObjects1[i].setAnimationName("Running");
+}
+}}
+
+}
+
+
+};gdjs.New_32sceneCode.eventsList1 = function(runtimeScene) {
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("Player"), gdjs.New_32sceneCode.GDPlayerObjects1);
+
+gdjs.New_32sceneCode.condition0IsTrue_0.val = false;
+{
+for(var i = 0, k = 0, l = gdjs.New_32sceneCode.GDPlayerObjects1.length;i<l;++i) {
+    if ( gdjs.New_32sceneCode.GDPlayerObjects1[i].getBehavior("PlatformerObject").isOnFloor() ) {
+        gdjs.New_32sceneCode.condition0IsTrue_0.val = true;
+        gdjs.New_32sceneCode.GDPlayerObjects1[k] = gdjs.New_32sceneCode.GDPlayerObjects1[i];
+        ++k;
+    }
+}
+gdjs.New_32sceneCode.GDPlayerObjects1.length = k;}if (gdjs.New_32sceneCode.condition0IsTrue_0.val) {
+
+{ //Subevents
+gdjs.New_32sceneCode.eventsList0(runtimeScene);} //End of subevents
+}
+
+}
+
+
+};
+
+gdjs.New_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.New_32sceneCode.GDPlayerObjects1.length = 0;
+gdjs.New_32sceneCode.GDPlayerObjects2.length = 0;
+gdjs.New_32sceneCode.GDPlayerObjects3.length = 0;
+gdjs.New_32sceneCode.GDTestBodenObjects1.length = 0;
+gdjs.New_32sceneCode.GDTestBodenObjects2.length = 0;
+gdjs.New_32sceneCode.GDTestBodenObjects3.length = 0;
+
+gdjs.New_32sceneCode.eventsList1(runtimeScene);
+return;
+
+}
+
+gdjs['New_32sceneCode'] = gdjs.New_32sceneCode;
